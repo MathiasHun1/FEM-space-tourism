f45b6ed5ee7afbedf85250a41b09ccee6d69a18e
snus4me <mathias.hun@gmail.com>
2025-03-10 21:34:09 +0100

    .

--- src/components/pages/Technology.jsx
+++ src/components/pages/Technology.jsx
@@ -1,29 +1,57 @@
 import image from '/images/technology/image-launch-vehicle-landscape.jpg';
 
+import data from '../../data.json';
+import { useState } from 'react';
+
 const Technology = () => {
+  const [currentData, setCurrentData] = useState(data.technology[0]);
+  const [activeTech, setActiveTech] = useState(data.technology[0].name);
+
+  const handleClick = (e) => {
+    const dataKey = e.currentTarget.dataset.tech;
+    const dataToRender = data.technology.find((t) => t.name === dataKey);
+
+    setCurrentData(dataToRender);
+    setActiveTech(dataToRender.name);
+  };
+
   return (
-    <div className="grid-container grid-container--technology wrapper flow flow-gap-lg">
+    <div className="grid-container grid-container--technology wrapper flow flow-gap-md">
       <h1 className="numbered-title text-preset-5 uppercase">
         <span>03</span>Space launch 101
       </h1>
 
       <div className="tecnology-image-wrapper">
-        <img src={image} alt="" />
+        <picture>
+          <source
+            srcset={currentData.images.landscape}
+            media="(max-width: 45rem)"
+          />
+
+          <img src={currentData.images.portrait} alt="" />
+        </picture>
       </div>
 
-      <article className="">
+      <article className="tech-article wrapper d-flex flow">
+        <div className="pagination-big d-flex gap-sm">
+          {data.technology.map((tech) => (
+            <button
+              key={tech.name}
+              data-tech={tech.name}
+              className={`${tech.name === activeTech ? 'active' : ''}`}
+              onClick={handleClick}
+            >
+              <span className="sr-only">{`The ${tech.name}`}</span>
+              {data.technology.indexOf(tech) + 1}
+            </button>
+          ))}
+        </div>
         <div className="flow flow-gap-sm">
-          <header className="">
+          <header className="flow flow-gap-sm">
             <h2 className="text-preset-4 uppercase">The terminology...</h2>
-            <p className="text-preset-3 uppercase nowrap">Space capsule</p>
+            <p className="text-preset-3 uppercase nowrap">{currentData.name}</p>
           </header>
-          <p className="text-preset-9 text-light">
-            A space capsule is an often-crewed spacecraft that uses a blunt-body
-            reentry capsule to reenter the Earth's atmosphere without wings. Our
-            capsule is where you'll spend your time during the flight. It
-            includes a space gym, cinema, and plenty of other activities to keep
-            you entertained.
-          </p>
+          <p className="text-preset-9 text-light">{currentData.description}</p>
         </div>
       </article>
     </div>
