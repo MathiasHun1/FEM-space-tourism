11b06636383b02c496af49af43ae70e4d8d50637
snus4me <mathias.hun@gmail.com>
2025-03-12 13:36:00 +0100

    tech-page layout

--- src/components/pages/Technology.jsx
+++ src/components/pages/Technology.jsx
@@ -16,23 +16,25 @@ const Technology = () => {
   };
 
   return (
-    <div className="grid-container--technology wrapper">
+    <div className="grid-container grid-container--technology">
       <h1 className="numbered-title text-preset-5 uppercase">
         <span>03</span>Space launch 101
       </h1>
 
-      <div className="tecnology-image-wrapper">
-        <picture>
-          <source
-            srcset={currentData.images.landscape}
-            media="(max-width: 45rem)"
-          />
+      <picture className="tecnology-image-wrapper">
+        <source
+          srcset={currentData.images.landscape}
+          media="(max-width: 80rem)"
+        />
+        <source
+          srcset={currentData.images.portrait}
+          media="(min-width: 80rem)"
+        />
 
-          <img src={currentData.images.portrait} alt="" />
-        </picture>
-      </div>
+        <img src={currentData.images.landscape} alt="" />
+      </picture>
 
-      <article className="tech-article wrapper d-flex">
+      <article className="tech-article d-flex">
         <div className="pagination-big d-flex gap-sm">
           {data.technology.map((tech) => (
             <button
@@ -49,9 +51,11 @@ const Technology = () => {
         <div className="">
           <header className="">
             <h2 className="text-preset-4 uppercase">The terminology...</h2>
-            <p className="text-preset-3 uppercase nowrap">{currentData.name}</p>
+            <p className="text-preset-3 uppercase ">{currentData.name}</p>
           </header>
-          <p className="text-preset-9 text-light">{currentData.description}</p>
+          <p className="tech-description text-preset-9 text-light">
+            {currentData.description}
+          </p>
         </div>
       </article>
     </div>
